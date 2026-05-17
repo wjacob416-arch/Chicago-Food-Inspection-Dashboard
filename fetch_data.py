aff30e6e2521a156d3ef785c5c4eec0804c531ff
wjacob416-arch <wjacob416@gmail.com>
2026-05-16 20:45:24 -0500

    Started api and data fetching

--- fetch_data.py
+++ fetch_data.py
@@ -1,3 +1,13 @@
 import requests
 import json
+import database
 
+api_url = "https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000"
+
+
+def fetch_url():
+    response = requests.get(api_url)
+    data = response.json()
+    return data
+
+def load_data():
